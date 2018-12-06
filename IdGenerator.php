8cf933337518fad4a3a55ff3cbdf14aeae97a1ab
alan.laplace <alan.laplace@gmail.com>
2018-12-06 20:59:20 -0300

    IdGenerator class created.

---
+++ IdGenerator.php
@@ -0,0 +1,31 @@
+<?php namespace SummaExercise;
+
+    class IdGenerator{
+	
+        private static $instance;
+        private $current_id = 0;
+    
+        private function __construct() {
+        }
+     
+        public static function getInstance()
+        {
+            if (!isset(self::$instance)) {
+                self::$instance = new IdGenerator();
+            } 
+            return self::$instance;
+        }
+        
+        public function getUniqueId()
+        {
+            $this->current_id ++;
+            return $this->current_id;
+        }
+       
+        public function __clone()
+        {
+            trigger_error('Clonation of this kind of object is not allowed', E_USER_ERROR);
+        }
+    }
+
+?>
